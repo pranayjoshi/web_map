a55fd8abf795f89f2a07d440897ec2098fbe4080
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2019-12-10 22:44:48 +0530

    Delete Extracter.py

--- pj/Extracter.py
+++
@@ -1,4 +0,0 @@
-#Created and Initiated by Pranay Joshi
-import pandas as pd
-data = pd.read_csv("data.csv", usecols=['laboratory', 'coordinate', 'city', 'country', 'contact'])
-data.to_csv(r'main.csv')
